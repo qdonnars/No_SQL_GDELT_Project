a1968e23232c5eb909207c5bd3e25a93f0658a85
qdonnars <qdonnars@gmail.com>
2018-01-17 11:11:59 +0100

    Maj api.py

--- api.py
+++ api.py
@@ -1,62 +1,78 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+import datetime as dt
 
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
+Country_list = ['FRANCE', 'ITALY', 'SPAIN']
+Horizon_considered = ['01-01-2016', '01-05-2016']
+Horizon_considered = [dt.datetime.strptime(date, '%d-%m-%Y').date() for date in Horizon_considered]
+
+Country_linked = {
+    'country1': {'country': 'FRANCE'},
+    'country2': {'country': 'SPAIN'},
+    'country3': {'country': 'ITALY'},
 }
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_country_doesnt_exist(country):
+    if country not in Country_list:
+        abort(404, message="Country {} doesn't exist".format(country))
+
+def abort_if_date_format_is_invalid(init_date, end_date):
+    try :
+        init_date = dt.datetime.strptime(init_date, '%d-%m-%Y')
+        end_date = dt.datetime.strptime(end_date, '%d-%m-%dY')
+    except ValueError:
+        abort(404, message="Date_format {} or {} doesn't exist".format(init_date, end_date))
+
+    if init_date < Horizon_considered[0]:
+        abort(404, message="Date {} too young".format(init_date))
+    if end_date > Horizon_considered[1]:
+        abort(404, message="Date {} too old".format(end_date))
+
 
 parser = reqparse.RequestParser()
-parser.add_argument('task')
-parser.add_argument('autre')
+parser.add_argument('country')
+parser.add_argument('init_date')
+parser.add_argument('end_date')
 
 
-# Todo
+# Requette
 # shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+class api_beahaviour(Resource):
     def get(self):
-        return TODOS
+        return {'message' : 'hello sunshine'} , 200
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        country = args['country']
+        init_date = args['init_date']
+        end_date = args['end_date']
+
+        abort_if_country_doesnt_exist(country)
+        abort_if_date_format_is_invalid(init_date, end_date)
+
+        ### create the request with pymongo
+
+
+        ### add value to the request
+
+
+        ### create the awnser
+        country = {'country': country}
+        init_date = {'init_date': init_date}
+        end_date = {'end_date': end_date}
+        #awnser = {country, init_date, end_date}
+        awnser = Country_linked
+        return awnser, 201
+
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(api_beahaviour, '/')
 
 
 if __name__ == '__main__':
