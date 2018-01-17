560696227ef0cdcf342d014231a357954d1b42f5
theoandro92 <35463250+theoandro92@users.noreply.github.com>
2018-01-17 16:00:26 +0000

    Add files via upload

--- calcul_imp.py
+++ calcul_imp.py
@@ -5,7 +5,8 @@ import unicodedata
 # Pour importer les donnees dans mongo : ./mongoimport --db test --collection data --drop --type csv --file /Users/caroline/Downloads/events_b.csv --headerline
 # On etablit la connection :
 def Impact(country1, country2, month):
-    [imp_pos, imp_neg, NumMen] = [0, 0, 0]
+    [imp_pos, imp_neg, mention] = [0, 0, 0]
+    month = month+201700
     # Taper le nom du host
     client = MongoClient()
     # On recupere le dataset
@@ -13,17 +14,10 @@ def Impact(country1, country2, month):
     # On recupere la collection
     col = db.events
     # On recupere les donnees de country1 sur country2 au mois month
-    table = db.data.find({'Actor1Geo_CountryCode':country1}, {'_id': 0},{'Actor1Geo_CountryCode':country1},{'MonthYear': '2017'+str(month)})
-    i=0
+    table = db.data.find({'Actor1Geo_CountryCode':country1, 'Actor2Geo_CountryCode':country2, 'MonthYear': month})
     for el in table:
-        if i==0:
-            df = pd.DataFrame(el,index=['0',])
-        else:
-            df = pd.concat([df,pd.DataFrame(el,index=[str(i),])])
-        i=i+1
-    for j in range(i):
-        Gold = df['GoldsteinScale'][j]
-        NumMen = df['NumMentions'][j]
+        Gold = el['GoldsteinScale']
+        NumMen = el['NumMentions']
         if Gold>0:
             imp_pos = imp_pos+(Gold*NumMen)
         else:
