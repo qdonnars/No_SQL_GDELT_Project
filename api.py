da27e3ef8252bf2bf3787eb09a7ddeb193883b34
theoandro <theoandro92@gmail.com>
2018-01-18 15:39:41 +0100

    api finale# Please enter the commit message for your changes. Lines starting

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 api = Api(app)
 
 # des listes permettant de tester la coherences des donnees en entree
-Country_list = ['FR', 'US', 'SP']
+Country_list = ['FR', 'US', 'EN', 'IT', 'GB']
 Horizon_considered = ['201701', '201712']
 
 Horizon_considered = [dt.datetime.strptime(date, '%Y%m') for date in Horizon_considered]
@@ -27,9 +27,9 @@ def abort_if_date_format_is_invalid(month):
     except ValueError:
         abort(404, message="Date_format {} or {} doesn't exist".format(month))
 
-    if month < Horizon_considered[0]:
+    if month <= Horizon_considered[0]:
         abort(404, message="Date {} too young".format(month))
-    if month > Horizon_considered[1]:
+    if month >= Horizon_considered[1]:
         abort(404, message="Date {} too old".format(month))
 
 
@@ -49,13 +49,13 @@ class api_beahaviour(Resource):
         def Impact(country1, country2, month):
             [imp_pos, imp_neg, mention] = [0, 0, 0]
             # Taper le nom du host
-            client = MongoClient()
+            client = MongoClient('mongodb://34.238.43.143:27025')
             # On recupere le dataset
-            db = client.test
+            db = client.gdelt
             # On recupere la collection
-            col = db.events
+            # col = db.events
             # On recupere les donnees de country1 sur country2 au mois month
-            table = db.data.find({'Actor1Geo_CountryCode':country1, 'Actor2Geo_CountryCode':country2, 'MonthYear': month})
+            table = db.events.find({'Actor1Geo_CountryCode':country1, 'Actor2Geo_CountryCode':country2, 'MonthYear': month})
             for el in table:
                 Gold = el['GoldsteinScale']
                 NumMen = el['NumMentions']
@@ -72,7 +72,9 @@ class api_beahaviour(Resource):
 
 
         ### create the awnser
-        awnser = {'imp1_C1_C2_pos': imp1_C1_C2_pos,
+        awnser = {'pays1' : country1,
+                  'pays2' : country2,
+                  'imp1_C1_C2_pos': imp1_C1_C2_pos,
                   'imp1_C1_C2_neg' : imp1_C1_C2_neg,
                   'mention_C1_C2' : mention_C1_C2,
                   'imp1_C2_C1_pos': imp1_C2_C1_pos,
@@ -82,15 +84,59 @@ class api_beahaviour(Resource):
         return awnser, 201
 
 class api_beahaviour2(Resource):
-    def get(self):
-        return {'message' : 'hello sunshine'} , 200
+    def get(self, country1, country2, month):
+        ### On enregistre en local les variables pour pouvoir traviller dessu
+        # args = parser.parse_args() # je crois qu'il n'y en a pas a parser
+
+        ### on test les variables   , init_date, end_date
+        abort_if_country_doesnt_exist(country1)
+        abort_if_country_doesnt_exist(country2)
+        abort_if_date_format_is_invalid(month)
+
+        ### create the request with pymongo (output : les donnees agregees)
+        def Impact(country1, country2, month):
+            [imp_pos, imp_neg, mention] = [0, 0, 0]
+            # Taper le nom du host
+            client = MongoClient('mongodb://34.238.43.143:27025')
+            # On recupere le dataset
+            db = client.gdelt
+            # On recupere la collection
+            # col = db.events
+            # On recupere les donnees de country1 sur country2 au mois month
+            table = db.events.find({'Actor1Geo_CountryCode':country1, 'Actor2Geo_CountryCode':country2, 'MonthYear': month})
+            for el in table:
+                Gold = el['GoldsteinScale']
+                NumMen = el['NumMentions']
+                if Gold>0:
+                    imp_pos = imp_pos+(Gold*NumMen)
+                else:
+                    imp_neg = imp_neg+(abs(Gold)*NumMen)
+                mention = mention+NumMen
+            return imp_neg, imp_pos, mention
+                
+        ### add value to the request
+        imp1_C1_C2_pos, imp1_C1_C2_neg, mention_C1_C2= Impact(country1, country2, int(month))
+        imp1_C2_C1_pos, imp1_C2_C1_neg, mention_C2_C1 = Impact(country2, country1, int(month))
 
 
+        ### create the awnser
+        awnser = {'pays1' : country1,
+                  'pays2' : country2,
+                  'imp1_C1_C2_pos': imp1_C1_C2_pos,
+                  'imp1_C1_C2_neg' : imp1_C1_C2_neg,
+                  'mention_C1_C2' : mention_C1_C2,
+                  'imp1_C2_C1_pos': imp1_C2_C1_pos,
+                  'imp1_C2_C1_neg' : imp1_C2_C1_neg,
+                  'mention_C2_C1' : mention_C2_C1
+                  }
+        return awnser, 201
+
+    
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(api_beahaviour, '/options/<country1>/<country2>/<month>')
-api.add_resource(api_beahaviour2, '/')
+api.add_resource(api_beahaviour2, '/<country1>/<country2>/<month>')
 
 ## je ne comprend pas cette commande
 if __name__ == '__main__':
